a58f3b2e33ea781094124956ce434ed02e559105
Pavi2007-pav <pr5074@srmist.edu.in>
2026-04-05 08:51:38 +0530

    Initial commit

---
+++ ArrayCreationAndAccess.java
@@ -0,0 +1,14 @@
+// Program Name: ArrayCreationAndAccess
+// Description: Create an array and access elements
+
+public class ArrayCreationAndAccess {
+    public static void main(String[] args) {
+
+        // Declare and initialize array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Access elements using index
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Second element: " + numbers[1]);
+    }
+}
