4a2982127fed5f51128f65cb98ef7bd684a3fad5
Pavithra1707-rt <pavithrart17@gmail.com>
2026-04-05 10:08:54 +0530

    level1iv

---
+++ ArrayLengthExample.java
@@ -0,0 +1,14 @@
+// Program Name: ArrayLengthExample
+// Description: Find size of array
+
+public class ArrayLengthExample {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30};
+
+        // Using length property
+        int size = numbers.length;
+
+        System.out.println("Array size: " + size);
+    }
+}
