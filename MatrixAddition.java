80f2aa718ba6c753ff3a88ba14f212f7df1c23d9
Pavithra1707-rt <pavithrart17@gmail.com>
2026-04-05 10:28:19 +0530

    level3ii

---
+++ MatrixAddition.java
@@ -0,0 +1,38 @@
+// Program Name: MatrixAddition
+// Description: Add two matrices
+
+public class MatrixAddition {
+    public static void main(String[] args) {
+
+        int[][] matrix1 = {
+            {1, 2},
+            {3, 4}
+        };
+
+        int[][] matrix2 = {
+            {5, 6},
+            {7, 8}
+        };
+
+        int rows = matrix1.length;
+        int cols = matrix1[0].length;
+
+        int[][] result = new int[rows][cols];
+
+        // Add matrices
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+
+        // Display result
+        System.out.println("Result Matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
